d05e837d95f15ff50dcad9da1ee10f0a60906a01
SysAdmin74 <60932480+SysAdmin74@users.noreply.github.com>
2024-02-04 14:57:53 +0800

    Update start.sh
    Added function for check web folder

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+check_web() {
+    echo "Checking if the 'web' folder is empty..."
+
+    if [ -z "$(ls -A /arozos/web)" ]; then
+        echo "'web' folder is empty. Extracting files from 'web.tar.gz'..."
+        tar -xzvf web.tar.gz -C /arozos/
+    else
+        echo "'web' folder is not empty."
+    fi
+}
+
+# Call the extra function
+check_web
+
 variable_list=("port" "hostname" "enable_hwman" "enable_pwman" "allow_autologin" "allow_cluster" "allow_iot" "allow_mdns" "allow_pkg_install" "allow_ssdp" "bufffile_size" "buffpool_size" "iobuf" "max_upload_size" "uuid")
 command="./launcher"
 for var_name in "${variable_list[@]}"; do
